e52187ff2fd11667227647abf6512f26d518154b
Lozumi Everkine <fantasynico1026@gmail.com>
2022-07-08 17:00:16 +0800

    Initial commit

---
+++ start.c
@@ -0,0 +1,20 @@
+/* 定义常量和函数 */
+#include <stdio.h>
+ 
+#define A 10
+#define B 5
+#define C 6
+int multiply(void);
+
+int main()
+{
+    int sum = multiply();
+    printf("Hello, World! \n" "The result is:%lu",sum);
+    return 0;
+}
+
+int multiply()
+{
+    int sum = A*B*C;
+    return sum;
+}
